08add179ee9dd947a5d728009cd35b4640f5003a
Milind <bhattmilindv@gmail.com>
2023-06-06 00:33:07 +0200

    a

---
+++ 4-names.js
@@ -0,0 +1,16 @@
+// shared 
+const john = "John";
+const peter = "Peter";
+//local: not shared
+const secret = "SUPER SECRET DOCUMENT";
+
+//console.log(module);
+/*
+Module has property 
+exports: {}
+in it.
+So whatever you dump in this export, will be exported by this file, and can be used by other files.
+*/
+module.exports = {john, peter};
+
+
