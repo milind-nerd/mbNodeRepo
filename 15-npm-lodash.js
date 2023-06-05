29e602fa02427e3de7b918fe77441a94e158625f
Milind <bhattmilindv@gmail.com>
2023-06-06 00:48:35 +0200

    Day 2

---
+++ 15-npm-lodash.js
@@ -0,0 +1,16 @@
+/* how can you access external modules? 
+First step, install them (see read-npm.doc)
+Next, define it like line 5
+*/
+const _ = require('lodash');
+
+const items = [1, [2, [3]]]
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
+
+/*
+IMP:
+Simply using "npm install lodash", like in video 28, gave error "lodash not found".
+Hence, use command npm install lodash --save to install Lodash
+*/ 
