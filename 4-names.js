b70844ff36a92fb6c35451863f4d3401de961820
Milind <bhattmilindv@gmail.com>
2023-06-15 15:18:40 +0200

    A

--- 4-names.js
+++ 4-names.js
@@ -10,7 +10,7 @@ Module has property
 exports: {}
 in it.
 So whatever you dump in this export, will be exported by this file, and can be used by other files.
-*/
+*/ 
 module.exports = {john, peter};
 
 
--- app.js
+++ app.js
@@ -3,13 +3,13 @@ First step, install them (see read-npm.doc)
 Next, define it like line 5
 */
 const _ = require('lodash');
-
+ 
 const items = [1, [2, [7]]]
 const newItems = _.flattenDeep(items);
 
 console.log(newItems);
 
-/*
+/* 
 IMP:
 Simply using "npm install lodash", like in video 28, gave error "lodash not found".
 Hence, use command npm install lodash --save to install Lodash
